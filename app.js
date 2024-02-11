fab96deafe78a81b39aab88f425d85f16fa4d74f
Karan Sewa <sewakaran221@gmail.com>
2024-02-11 12:05:02 +0530

    Background mode changer

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+document.querySelector("button").addEventListener("click", () => {
+  document.querySelector("body").classList.toggle("toggleStyle");
+  document.querySelector("nav").classList.toggle("nav-bg");
+});
